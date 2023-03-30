e21f3d9802686fdcd87cdf83be69e9d1dd15d98e
dsalato <d.salato@mail.ru>
2023-03-30 07:04:11 +0300

    Implementing access rights

--- core/Src/Auth/Auth.php
+++ core/Src/Auth/Auth.php
@@ -7,7 +7,6 @@ class Auth
 {
     //Свойство для хранения любого класса, реализующего интерфейс IdentityInterface
     private static IdentityInterface $user;
-
     //Инициализация класса пользователя
     public static function init(IdentityInterface $user): void
     {
@@ -16,14 +15,12 @@ class Auth
             self::login(self::user());
         }
     }
-
     //Вход пользователя по модели
     public static function login(IdentityInterface $user): void
     {
         self::$user = $user;
         Session::set('id', self::$user->getId());
     }
-
     //Аутентификация пользователя и вход по учетным данным
     public static function attempt(array $credentials): bool
     {
@@ -33,14 +30,12 @@ class Auth
         }
         return false;
     }
-
     //Возврат текущего аутентифицированного пользователя
     public static function user()
     {
         $id = Session::get('id') ?? 0;
         return self::$user->findIdentity($id);
     }
-
     //Проверка является ли текущий пользователь аутентифицированным
     public static function check(): bool
     {
